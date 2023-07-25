41434a8bf35237b316c832e9b6a05279b7005ae1
nozcat <overclockednoz@proton.me>
2023-07-25 00:49:08 -0600

    ringbuf channels

--- src/main.rs
+++ src/main.rs
@@ -7,45 +7,64 @@ const N: usize = 1_000_000_000;
 fn main() {
     let start = Instant::now();
 
-    let (generator_to_sha512_tx, generator_to_sha512_rx) = flume::bounded(1_000_000);
-    let (generator_to_blake3_tx, generator_to_blake3_rx) = flume::bounded(1_000_000);
-    let (sha512_to_result_tx, sha512_to_result_rx) = flume::bounded(1_000_000);
-    let (blake3_to_result_tx, blake3_to_result_rx) = flume::bounded(1_000_000);
+    let (mut generator_to_sha512_tx, mut generator_to_sha512_rx) =
+        ringbuf::HeapRb::new(1_000_000).split();
+    let (mut generator_to_blake3_tx, mut generator_to_blake3_rx) =
+        ringbuf::HeapRb::new(1_000_000).split();
+    let (mut sha512_to_result_tx, mut sha512_to_result_rx) =
+        ringbuf::HeapRb::new(1_000_000).split();
+    let (mut blake3_to_result_tx, mut blake3_to_result_rx) =
+        ringbuf::HeapRb::new(1_000_000).split();
 
     // Generator
     thread::spawn(move || {
         for i in 0..N {
             let preimage = (i as u64).to_le_bytes();
-            generator_to_sha512_tx.send(preimage.clone()).unwrap();
-            generator_to_blake3_tx.send(preimage).unwrap();
+            push(&mut generator_to_sha512_tx, preimage.clone());
+            push(&mut generator_to_blake3_tx, preimage);
         }
     });
 
     // Sha512
-    thread::spawn(move || {
-        while let Ok(preimage) = generator_to_sha512_rx.recv() {
-            let hash = Sha512::digest(&preimage);
-            sha512_to_result_tx.send(hash).ok();
-        }
+    thread::spawn(move || loop {
+        let preimage = pop(&mut generator_to_sha512_rx);
+        let hash = Sha512::digest(&preimage);
+        push(&mut sha512_to_result_tx, hash);
     });
 
     // Blake3
-    thread::spawn(move || {
-        while let Ok(preimage) = generator_to_blake3_rx.recv() {
-            let hash = blake3::hash(&preimage);
-            blake3_to_result_tx.send(hash).ok();
-        }
+    thread::spawn(move || loop {
+        let preimage = pop(&mut generator_to_blake3_rx);
+        let hash = blake3::hash(&preimage);
+        push(&mut blake3_to_result_tx, hash);
     });
 
     // Result
     let result_thread = thread::spawn(move || {
         for _ in 0..N {
-            sha512_to_result_rx.recv().unwrap();
-            blake3_to_result_rx.recv().unwrap();
+            pop(&mut sha512_to_result_rx);
+            pop(&mut blake3_to_result_rx);
         }
     });
 
     result_thread.join().unwrap();
 
     println!("{:?}", start.elapsed());
+}
+
+fn push<T>(tx: &mut ringbuf::HeapProducer<T>, mut value: T) {
+    loop {
+        match tx.push(value) {
+            Ok(_) => break,
+            Err(v) => value = v,
+        }
+    }
+}
+
+fn pop<T>(rx: &mut ringbuf::HeapConsumer<T>) -> T {
+    loop {
+        if let Some(value) = rx.pop() {
+            return value;
+        }
+    }
 }
